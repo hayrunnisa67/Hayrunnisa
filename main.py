3dd7bc0b7d765937497bcb5fd91b52562e2e9382
hayrunnisa67 <hayrunnisa.muslu@std.yeditepe.edu.tr>
2024-12-26 17:32:09 +0300

    HAYRUNNİSA MUSLU 26.12.2024
    DERS İÇİ ÇALIŞMA

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-alter=int(input("Alter:"))
-note=int(input("Note:"))
-if 20<=alter <=50 and note >80:
-   print ("einstellen")
+punktzahl=float(input("punktzahl:"))
+programmiererfahrung=int(input("programmiererfahrung (1-5)"))
+
+if punktzahhl > 90 or (programmiererfahrung==5 and punktzahl>=70): 
+   print("einstellung")
+elif punktzahl >= 70 or programmiererfahrung == 4:
+  print ("zum Gesprach einladen1")
 else:
-  print("ablehnen")
+       print ("ablehnen")
