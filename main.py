10538241ea171999f5921076382a8d5a73f2502b
hayrunnisa67 <hayrunnisa.muslu@std.yeditepe.edu.tr>
2025-01-02 17:04:39 +0300

    mesafe ölçer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x1=int(input("x1"))
-y1=int(input("y1"))
-x2=int(input("x2"))
-y2=int(input("x2"))
-d=y2-y1//x2-x1
-print(d)
+x1=int(input("x1="))
+y1=int(input("y1="))
+x2=int(input("x2="))
+y2=int(input("x2="))
+d=((x2-x1)**2+(y2-y1)**2)**(1/2)
+print("d=" + str(d))
